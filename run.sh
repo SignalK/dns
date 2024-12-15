09f042f40793c9e472238dd1e3b14f09869137a1
Tim Mathews <tim@signalk.org>
2024-12-15 15:20:45 -0500

    Configure run.sh
    Set up environment variable passthrough in run.sh so that API user and
    token can be set from command line.
    
    run.sh can take an argument and pass it to dnsconfig as shown in
    README.md.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -it -v $(pwd)/dnsconfig.js:/dns/dnsconfig.js -v $(pwd)/creds.json:/dns/creds.json --entrypoint /bin/sh stackexchange/dnscontrol:4.15.1
+docker run -e TOKEN=$TOKEN -e USER=$USER --rm -it -v $(pwd)/dnsconfig.js:/dns/dnsconfig.js -v $(pwd)/creds.json:/dns/creds.json stackexchange/dnscontrol:4.15.1 $1
